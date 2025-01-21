11788ab4de334015e2ba2ebd24d03c88c97cfd5f
samir jadhav <samirzjadhav@gmail.com>
2025-01-21 21:13:38 +0530

    feat: added new account section and sign in button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import { brainwave } from "../assets/";
 import { navigation } from "../constant";
 import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 const Header = () => {
   const pathName = useLocation();
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         {" "}
         <a href="#hero" className="block w-[12rem] xl:mr-8">
@@ -31,6 +32,15 @@ const Header = () => {
             ))}
           </div>
         </nav>
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
       </div>
     </div>
   );

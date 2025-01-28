21743b487fba86383fd98580bd1871fb6ff493b5
samir jadhav <samirzjadhav@gmail.com>
2025-01-28 06:57:29 +0530

    fix: hero section hero image and a content on it

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,10 @@ import React from "react";
 import Section from "./Section";
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constant";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 const Hero = () => {
   return (
@@ -39,27 +43,26 @@ const Hero = () => {
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
-              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]">
-                <div className="aspect-[33/40] rounded-b-[0.9] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
-                  <img
-                    src={robot}
-                    alt="Ai"
-                    className="w-full"
-                    width={1024}
-                    height={490}
-                  />
-                </div>
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  alt="Ai"
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%] "
+                  width={1024}
+                  height={490}
+                />
               </div>
             </div>
-            <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-              <img
-                src={heroBackground}
-                alt="hero"
-                className="w-full"
-                width={1440}
-                height={1800}
-              />
-            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              alt="hero"
+              className="w-full"
+              width={1440}
+              height={1800}
+            />
           </div>
         </div>
       </div>

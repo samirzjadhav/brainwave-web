351854b14b9de2e7d6e2999aa9b2451d9c9b8437
samir jadhav <samirzjadhav@gmail.com>
2025-01-29 18:33:45 +0530

    feat: added new background with animation and button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,8 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -16,7 +18,7 @@ const Hero = () => {
       customPadding
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of AI Chatting with{" "}
@@ -52,8 +54,18 @@ const Hero = () => {
                   width={1024}
                   height={490}
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl lg:flex xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} alt="" width={24} height={25} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -64,6 +76,7 @@ const Hero = () => {
               height={1800}
             />
           </div>
+          <BackgroundCircles />
         </div>
       </div>
     </Section>

648161353d8916513339a84f3246588b5b35cb61
samir jadhav <samirzjadhav@gmail.com>
2025-01-31 15:40:47 +0530

    feat: added new componylogo that defined logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,7 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
 import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -87,6 +88,7 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
       <BottomLine />
     </Section>

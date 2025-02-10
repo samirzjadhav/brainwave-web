69ca729cb5d7d35b9f768e0388730e59720309a5
samir jadhav <samirzjadhav@gmail.com>
2025-02-10 10:26:18 +0530

    feat: added new card component for a greater look on it

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -2,6 +2,8 @@ import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "../constant";
+import Arrow from "../assets/svg/Arrow";
+import GradientLight from "./design/Benefits";
 
 const Benefits = () => {
   return (
@@ -13,10 +15,29 @@ const Benefits = () => {
         />
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
-            <div className="" key={item.id}>
-              <div className="">
-                <h5>{item.title}</h5>
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-cover bg-center md:max-w-[24rem] h-[300px]"
+              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{item.title}</h5>
+                <p>{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase">
+                    Explore More
+                  </p>
+                  <Arrow />
+                </div>
               </div>
+              {item.light && <GradientLight />}
+              
             </div>
           ))}
         </div>
--- src/components/design/Benefits.jsx
+++ src/components/design/Benefits.jsx
@@ -1,5 +1,9 @@
-export const GradientLight = () => {
+import React from "react";
+
+const GradientLight = () => {
   return (
     <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />
   );
 };
+
+export default GradientLight;

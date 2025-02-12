f6d9a009e7e04eb3078bb849e9522a4d5f132e53
samir jadhav <samirzjadhav@gmail.com>
2025-02-12 12:01:38 +0530

    feat: added right side circle on the collaboration section style

--- src/components/Collaboration.js
+++ src/components/Collaboration.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Section from "./Section";
-import { collabContent } from "../constant";
-import { check } from "../assets";
+import { collabApps, collabContent, collabText } from "../constant";
+import { brainwaveSymbol, check } from "../assets";
 import Button from "./Button";
 
 const Collaboration = () => {
@@ -26,6 +26,33 @@ const Collaboration = () => {
             ))}
           </ul>
           <Button>Try it Now</Button>
+        </div>
+        <div className="lg:ml-auto xl:w-[38rem]">
+          <p className="body-2 mb-4 text-n-4 md:mb-16 ld:mb-32 lg:w-[22rem] lg:mx-auto">
+            {collabText}
+          </p>
+
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  <img
+                    src={brainwaveSymbol}
+                    alt="brainwave"
+                    width={48}
+                    height={48}
+                  />
+                </div>
+              </div>
+            </div>
+            <ul>
+              {collabApps.map((item, app) => (
+                <li key={app.id}>
+                  <div className=""></div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </Section>

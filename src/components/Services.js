7b0c594ef3cb9f2c9b168eee3ac80f70bdc3d1cd
samir jadhav <samirzjadhav@gmail.com>
2025-02-12 12:38:49 +0530

    feat: added service title and text and also props for heading component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Section from "./Section";
+import Heading from "./Heading";
 
 const Services = () => {
-  return <Section>Services</Section>;
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI-powered applications"
+        />
+      </div>
+    </Section>
+  );
 };
 
 export default Services;

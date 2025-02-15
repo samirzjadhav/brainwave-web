11724e3b0566bb02c10396015f72bc9ad6af6ab3
samir jadhav <samirzjadhav@gmail.com>
2025-02-15 17:28:13 +0530

    feat: added price list section and make a card and also style it

--- src/components/PricingList.js
+++ src/components/PricingList.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { pricing } from "../constant";
+import Button from "./Button";
+import { check } from "../assets";
 
 const PricingList = () => {
   return (
-    <div>PricingList</div>
-  )
-}
+    <div className="flex gap-[1rem] max-lg:flex-wrap">
+      {pricing.map((item) => (
+        <div
+          key={item.id}
+          className="w-[19rem] max-lg:w-full h-full px-6 lg:w-auto bg-n-8 border border-n-6 rounded-[2rem] even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:odd:text-color-3"
+          id={item.id}
+        >
+          <h4 className="h4 mb-4">{item.title}</h4>
+          <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">
+            {item.description}
+          </p>
+          <div className="flex items-center h-[5.5rem] mb-6">
+            {item.price && (
+              <>
+                <div className="h3">$</div>
+                <div className="text-[5.5rem] leading-none font-bold">
+                  {item.price}
+                </div>
+              </>
+            )}
+          </div>
 
-export default PricingList
+          <Button
+            className="w-full mb-6"
+            href={item.price ? "/pricing" : "/home"}
+            white={!!item.price}
+          >
+            {item.price ? "Get Started" : "Contact Us"}
+          </Button>
+          <ul>
+            {item.features.map((feature, index) => (
+              <li
+                key={index}
+                className="flex items-start py-5 border-t border-n-6"
+              >
+                <img src={check} width={24} height={24} alt="check" />
+                <p className="body-2 ml-4">{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PricingList;
